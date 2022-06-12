485e79fe9e34fefe3777990d13dc82e8b1d6860e
Sihan <u6569267@anu.edu.au>
2022-06-12 23:20:50 +1000

    Update mod structure

--- src/main/kotlin/io/github/nbcss/wynnlib/WynnLib.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/WynnLib.kt
@@ -2,7 +2,7 @@ package io.github.nbcss.wynnlib
 import net.fabricmc.api.ModInitializer
 @Suppress("UNUSED")
 object WynnLib: ModInitializer {
-    private const val MOD_ID = "mod_id"
+    private const val MOD_ID = "wynnlib"
     override fun onInitialize() {
         println("Example mod has been initialized.")
     }
