8470a8625a001db1a7cecbbf01604dc11fa8c250
Sihan <u6569267@anu.edu.au>
2022-06-12 23:10:35 +1000

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("fabric-loom")
+    val kotlinVersion: String by System.getProperties()
+    kotlin("jvm").version(kotlinVersion)
+}
+base {
+    val archivesBaseName: String by project
+    archivesName.set(archivesBaseName)
+}
+val modVersion: String by project
+version = modVersion
+val mavenGroup: String by project
+group = mavenGroup
+repositories {}
+dependencies {
+    val minecraftVersion: String by project
+    minecraft("com.mojang", "minecraft", minecraftVersion)
+    val yarnMappings: String by project
+    mappings("net.fabricmc", "yarn", yarnMappings, null, "v2")
+    val loaderVersion: String by project
+    modImplementation("net.fabricmc", "fabric-loader", loaderVersion)
+    val fabricVersion: String by project
+    modImplementation("net.fabricmc.fabric-api", "fabric-api", fabricVersion)
+    val fabricKotlinVersion: String by project
+    modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion)
+}
+tasks {
+    val javaVersion = JavaVersion.VERSION_17
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+        options.release.set(javaVersion.toString().toInt())
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions { jvmTarget = javaVersion.toString() }
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+    }
+    jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
+    processResources {
+        inputs.property("version", project.version)
+        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
+    }
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        withSourcesJar()
+    }
+}
---
+++ src/main/kotlin/io/github/nbcss/wynnlib/WynnLib.kt
@@ -0,0 +1,9 @@
+package io.github.nbcss.wynnlib
+import net.fabricmc.api.ModInitializer
+@Suppress("UNUSED")
+object WynnLib: ModInitializer {
+    private const val MOD_ID = "mod_id"
+    override fun onInitialize() {
+        println("Example mod has been initialized.")
+    }
+}
