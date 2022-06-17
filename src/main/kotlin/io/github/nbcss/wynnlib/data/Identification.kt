aeb4dee5ec76af076a434c0f2c88c4f936e47b8d
Sihan <u6569267@anu.edu.au>
2022-06-17 17:22:25 +1000

    Add Identification lang

--- src/main/kotlin/io/github/nbcss/wynnlib/data/Identification.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/data/Identification.kt
@@ -3,7 +3,7 @@ package io.github.nbcss.wynnlib.data
 import com.google.gson.JsonObject
 import io.github.nbcss.wynnlib.utils.Keyed
 
-data class Identification(val name: String,             //name used in equipment api data
+data class Identification(val id: String,               //id used in equipment api data
                           val innerName: String,        //name used in ingredient api data
                           val displayName: String,      //display name in game
                           val translationKey: String,   //key to translate
@@ -12,12 +12,23 @@ data class Identification(val name: String,             //name used in equipment
                           val constant: Boolean         //whether the id range is always fixed to base
                           ): Keyed {
     constructor(json: JsonObject) : this(
-        json.get("name").asString,
+        json.get("id").asString,
         json.get("innerName").asString,
         json.get("displayName").asString,
         json.get("translationKey").asString,
         json.get("suffix").asString,
         if(json.has("inverted")) json.get("inverted").asBoolean else false,
         if(json.has("constant")) json.get("constant").asBoolean else false)
-    override fun getKey(): String = name
+    override fun getKey(): String = id
+
+    enum class Group {
+        SKILL_POINT_BONUS,
+        COMBAT,
+        SURVIVABILITY,
+        ELEMENT_DAMAGE_BONUS,
+        ELEMENT_DEFENCE_BONUS,
+        MANEUVERABILITY,
+        MISC,
+        SPELL_COST,
+    }
 }
--- src/main/kotlin/io/github/nbcss/wynnlib/items/regular/RegularEquipment.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/items/regular/RegularEquipment.kt
@@ -43,8 +43,8 @@ class RegularEquipment(json: JsonObject) : Equipment {
                 spMap[it] = value
             }
         }
-        Metadata.getIdentifications().filter{json.has(it.name)}.forEach{
-            val value = json.get(it.name).asInt
+        Metadata.getIdentifications().filter{json.has(it.id)}.forEach{
+            val value = json.get(it.id).asInt
             if(value != 0)
                 idMap[it] = value
         }
