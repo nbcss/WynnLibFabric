3e3ed6d1979d671d447f6d51c14b820b56a6a64d
Sihan <u6569267@anu.edu.au>
2022-06-26 14:48:04 +1000

    Add Recipe class

---
+++ src/main/kotlin/io/github/nbcss/wynnlib/items/Recipe.kt
@@ -0,0 +1,24 @@
+package io.github.nbcss.wynnlib.items
+
+import com.google.gson.JsonObject
+import io.github.nbcss.wynnlib.utils.Color
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+
+class Recipe(json: JsonObject): BaseItem {
+    override fun getDisplayText(): Text {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDisplayName(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getIcon(): ItemStack {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRarityColor(): Color {
+        TODO("Not yet implemented")
+    }
+}
