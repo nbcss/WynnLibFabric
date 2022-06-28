307055daf3818022e0cf8a0a6c2b0bdeebf07414
Sihan <u6569267@anu.edu.au>
2022-06-28 21:42:10 +1000

    Update Range floating point display

--- src/main/kotlin/io/github/nbcss/wynnlib/abilities/display/RangeDisplay.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/abilities/display/RangeDisplay.kt
@@ -11,7 +11,7 @@ object RangeDisplay: EffectDisplay {
     override fun getTooltip(provider: PropertyProvider): List<Text> {
         RangeProperty.read(provider)?.let {
             val value = (if(it <= 1) Translations.TOOLTIP_SUFFIX_BLOCK else Translations.TOOLTIP_SUFFIX_BLOCKS)
-                .formatted(Formatting.WHITE, null, it)
+                .formatted(Formatting.WHITE, null, if (it % 1.0 != 0.0) it else it.toInt())
             return listOf(Symbol.RANGE.asText().append(" ")
                 .append(Translations.TOOLTIP_ABILITY_RANGE.formatted(Formatting.GRAY).append(": "))
                 .append(value))
