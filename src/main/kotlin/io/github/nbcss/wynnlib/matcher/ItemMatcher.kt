502f11f811329c2fa5e529308693e3a575eae43e
Sihan <u6569267@anu.edu.au>
2022-07-02 19:42:40 +1000

    Update comment of ItemMatcher

--- src/main/kotlin/io/github/nbcss/wynnlib/matcher/ItemMatcher.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/matcher/ItemMatcher.kt
@@ -11,6 +11,14 @@ import java.util.function.Supplier
 
 interface ItemMatcher {
     //fun toBaseItem(item: ItemStack): BaseItem?
+    /**
+     * Match item to rarity color provider.
+     * If the item cannot match with an associated color provider, the method will return null.
+     *
+     * @param item the item stack
+     * @param tooltip the tooltip of the itemstack (you cannot and should not modify it)
+     * @return color supplier, or null if the given item does not have associated color in the matcher.
+     */
     fun toRarityColor(item: ItemStack, tooltip: List<Text>): Supplier<Color?>?
 
     companion object {
