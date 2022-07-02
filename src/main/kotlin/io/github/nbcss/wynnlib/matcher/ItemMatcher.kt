fc2e08700cf9969799f3cff0923a6a233eb3030d
Sihan <u6569267@anu.edu.au>
2022-07-02 15:05:32 +1000

    Add draft implementation of background slot color

--- src/main/kotlin/io/github/nbcss/wynnlib/matcher/ItemMatcher.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/matcher/ItemMatcher.kt
@@ -1,7 +1,11 @@
 package io.github.nbcss.wynnlib.matcher
 
+import io.github.nbcss.wynnlib.Settings
+import io.github.nbcss.wynnlib.data.Tier
 import io.github.nbcss.wynnlib.items.BaseItem
 import io.github.nbcss.wynnlib.utils.Color
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.ItemStack
 
 interface ItemMatcher {
@@ -18,10 +22,20 @@ interface ItemMatcher {
             //println(item.name)
             if (item.isEmpty)
                 return null
+            val tooltip = item.getTooltip(MinecraftClient.getInstance().player, TooltipContext.Default.NORMAL)
             //todo
+            tooltip.forEach {
+                val s = it.string
+                //println(s)
+                for (tier in Tier.values()) {
+                    if (s.contains(tier.displayName)){
+                        return Settings.getTierColor(tier)
+                    }
+                }
+            }
             //val colors = listOf(Color.DARK_PURPLE, Color.RED, Color.AQUA, Color.PINK, Color.YELLOW)
             //return colors.random()
-            return Color.AQUA
+            return null
         }
     }
 }
