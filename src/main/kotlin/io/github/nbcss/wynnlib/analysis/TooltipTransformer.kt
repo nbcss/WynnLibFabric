9fe1dad3f69d42090b36075df76880c9e828568f
Sihan <u6569267@anu.edu.au>
2022-07-20 19:54:58 +1000

    Add transformer cache

--- src/main/kotlin/io/github/nbcss/wynnlib/analysis/TooltipTransformer.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/analysis/TooltipTransformer.kt
@@ -3,18 +3,27 @@ package io.github.nbcss.wynnlib.analysis
 import io.github.nbcss.wynnlib.items.BaseItem
 import net.minecraft.client.MinecraftClient
 import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
 import net.minecraft.text.Text
+import java.util.*
 
 interface TooltipTransformer {
     fun getTooltip(): List<Text>
 
     companion object {
+        private val transformerCacheMap: MutableMap<String, TooltipTransformer?> = WeakHashMap()
         private val factories: List<Factory> = listOf(
             EquipmentTransformer
         )
 
         fun asTransformer(stack: ItemStack, item: BaseItem): TooltipTransformer? {
-            return factories.firstNotNullOfOrNull { it.create(stack, item) }
+            val key = stack.writeNbt(NbtCompound()).toString()
+            if (transformerCacheMap.containsKey(key)) {
+                return transformerCacheMap[key]
+            }
+            val result = factories.firstNotNullOfOrNull { it.create(stack, item) }
+            transformerCacheMap[key] = result
+            return result
         }
     }
 
