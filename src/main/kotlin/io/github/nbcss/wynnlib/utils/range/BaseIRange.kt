1fe6899a901a97595e0bbf4001c9941334f1f910
Sihan <u6569267@anu.edu.au>
2022-07-23 15:25:56 +1000

    update data

--- src/main/kotlin/io/github/nbcss/wynnlib/utils/range/BaseIRange.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/utils/range/BaseIRange.kt
@@ -7,20 +7,17 @@ class BaseIRange(private val id: Identification, private val base: Int): IRange 
 
     fun base(): Int = base
 
-
     private fun getLower(): Int {
-        return when {
-            base>0 -> max(1, (base*0.3).roundToInt()) // Fix when base is 1 which should return 1 but returns 0
-            else -> (base*1.3).roundToInt()
-        }
+        return if (base>0)
+            max(1, (base*0.3).roundToInt()) // Fix when base is 1 which should return 1 but returns 0
+        else (base*1.3).roundToInt()
     }
 
     private fun getUpper(): Int {
-        return when {
-            base > 0 -> (base * 1.3).roundToInt()
-            else -> (base * 0.7).roundToInt()
-        }
+        return if (base > 0) (base * 1.3).roundToInt()
+        else (base * 0.7).roundToInt()
     }
+
     override fun lower(): Int {
         return when{
             id.constant -> base
