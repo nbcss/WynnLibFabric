e1f94fadf71396babb89e320993ba2f3629f27da
Sihan <u6569267@anu.edu.au>
2022-10-21 22:34:04 +1100

    (Maybe) fixed icon timer resolution issue

--- src/main/java/io/github/nbcss/wynnlib/mixins/timer/TimerHUDMixin.java
+++ src/main/java/io/github/nbcss/wynnlib/mixins/timer/TimerHUDMixin.java
@@ -1,5 +1,6 @@
 package io.github.nbcss.wynnlib.mixins.timer;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.nbcss.wynnlib.Settings;
 import io.github.nbcss.wynnlib.timer.IconIndicator;
 import io.github.nbcss.wynnlib.timer.SideIndicator;
@@ -9,6 +10,7 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -57,6 +59,8 @@ public abstract class TimerHUDMixin {
         //unit = 28 per timer
         int posX = client.getWindow().getScaledWidth() / 2 - timers.size() * 14;
         int posY = client.getWindow().getScaledHeight() - 108;
+        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
         for (IconIndicator timer : timers) {
             timer.render(matrices, getTextRenderer(), posX, posY, delta);
             posX += 28;
